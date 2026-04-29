1754d9dd3cac8d39555f4f675f3c22a900840589
Allison Wilson <somehesherkid@gmail.com>
2026-04-29 13:44:55 -0600

    Add back end test folder

---
+++ Book-Exchange.Tests/BackEnd/TransactionTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Moq;
+
+// Transaction Tests 
+// Covers: UT-TRANS-01 through UT-TRANS-05 (Unit Tests)
+//        IT-TRANS-01 through IT-TRANS-03 (Integration Tests)
+namespace Book_Exchange.Tests.Transaction;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
