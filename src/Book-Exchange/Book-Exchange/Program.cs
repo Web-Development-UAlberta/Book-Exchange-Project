cded49d4fd5c66131f5e0525717760b4eb310287
Allison Wilson <somehesherkid@gmail.com>
2026-05-03 11:08:02 -0600

    Add review business logic framework, service, dtos, transaction controller

--- src/Book-Exchange/Book-Exchange/Program.cs
+++ src/Book-Exchange/Book-Exchange/Program.cs
@@ -33,10 +33,12 @@ builder.Services
 
 builder.Services.AddControllersWithViews();
 
+// TODO: uncomment once services are implemented
 // builder.Services.AddScoped<IExchangeRequestService, ExchangeRequestService>();
 builder.Services.AddScoped<IListingService, ListingService>();
 builder.Services.AddScoped<IMessageService, MessageService>();
 builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
 builder.Services.AddScoped<IShippingService, ShippingService>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IWishlistService, WishlistService>();
