389ab96985439af0c88a4287043af927394205d8
Allison Wilson <somehesherkid@gmail.com>
2026-05-07 11:04:22 -0600

    Uncomment Message Unit Tests they pass

--- src/Book-Exchange/Book-Exchange/Services/Interfaces/IReviewService.cs
+++ src/Book-Exchange/Book-Exchange/Services/Interfaces/IReviewService.cs
@@ -2,7 +2,7 @@ using Book_Exchange.Models;
 using Book_Exchange.Models.DTOs.Review;
 
 namespace Book_Exchange.Services.Interfaces;
-// TODO: Once ORM is implemented make sure nothing changes. 
+
 public interface IReviewService
 {
     Task<Review> CreateReviewAsync(CreateReviewDto dto, Guid reviewerId);
