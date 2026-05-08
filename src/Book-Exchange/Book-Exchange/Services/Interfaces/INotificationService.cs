55078e57b6cb301c267b4de353a4509b977a7ec1
Allison Wilson <somehesherkid@gmail.com>
2026-05-08 17:39:08 -0600

    Add notification Service, views, and run integration tests

--- src/Book-Exchange/Book-Exchange/Services/Interfaces/INotificationService.cs
+++ src/Book-Exchange/Book-Exchange/Services/Interfaces/INotificationService.cs
@@ -2,7 +2,7 @@ using Book_Exchange.Models;
 using Book_Exchange.Models.DTOs.Notification;
 
 namespace Book_Exchange.Services.Interfaces;
-// TODO: Once ORM is implemented make sure nothing changes. 
+
 public interface INotificationService
 {
     Task<NotificationDto> GetNotificationByIdAsync(Guid notificationId, Guid userId);
